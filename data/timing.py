a8f96a393dd44e11f5f99c2f99cc8152cdcc065d
Kaif Khan <kaif-00z@proton.me>
2025-08-27 14:55:13 +0530

    Audited Menu & Timing

---
+++ data/timing.py
@@ -0,0 +1,8 @@
+# too lazy to make a json file
+
+TIMING = {
+    "BREAKFAST": "07:15 AM - 09:15 AM",
+    "LUNCH": "12:30 PM - 2:30 PM",
+    "SNACKS": "5:30 PM - 7:00 PM",
+    "DINNER": "8:30 PM - 10:00 PM"
+}
